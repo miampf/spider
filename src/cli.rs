3aaaead9770af53f05584ed23e19f4351901ecd6
miampf <miampf@proton.me>
2023-06-04 18:36:40 +0200

    made output optional

--- src/cli.rs
+++ src/cli.rs
@@ -12,6 +12,6 @@ pub struct Args {
     #[clap(short, long, help="Save all files found. This will create a new directory with the website name.")]
     save_files: bool,
     #[clap(short, long, help="A filename to which the valid URLs are written. This also saves email addresses if showing mail addresses is enabled.")]
-    output: String,
+    output: Option<String>,
     url: String
 }
